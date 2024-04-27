9b8da1a0651b793b98c5a107a68311da0f28aad5
Piotr Jura <jura.piotrek@gmail.com>
2024-04-27 21:48:06 +0200

    0031-tailwind-and-cleanup

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -1,47 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl font-medium">Event Booking App</h1>
+    <h2 class="text-2xl font-medium">All Events</h2>
+    <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
