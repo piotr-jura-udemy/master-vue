fb9b985f8f7113d88eb23d4a73d8b53df200fc55
Piotr Jura <jura.piotrek@gmail.com>
2024-04-28 13:29:24 +0200

    0032-first-component

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -2,6 +2,11 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
+    <EventCard />
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
 </template>
+
+<script setup>
+import EventCard from '@/components/EventCard.vue';
+</script>
