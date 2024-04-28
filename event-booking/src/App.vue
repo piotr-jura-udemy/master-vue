879e7a986cea7bd03c839bed71f124fa8a2ad0c1
Piotr Jura <jura.piotrek@gmail.com>
2024-04-28 18:44:07 +0200

    0033-list-of-components

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -2,7 +2,9 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <EventCard />
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard v-for="i in 8" :key="i" />
+    </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
 </template>
--- event-booking/src/components/EventCard.vue
+++ event-booking/src/components/EventCard.vue
@@ -1,3 +1,7 @@
 <template>
-  <div>Component!</div>
+  <div class="border border-gray-200 bg-white rounded-md">
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">What</h3>
+    <p class="p-4 border-b border-gray-200">When</p>
+    <p class="p-4">Description</p>
+  </div>
 </template>
