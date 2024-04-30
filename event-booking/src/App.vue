04edbaf15d9c804d2ad9dec5cb4840baf7719662
Piotr Jura <jura.piotrek@gmail.com>
2024-04-30 15:47:01 +0200

    0034-defining-props

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -3,7 +3,8 @@
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard v-for="i in 8" :key="i" />
+      <EventCard v-for="i in 8" :key="i" title="Vue Conference 2024" when="2024-05-01"
+        description="Conference about Vue and JavaScript" />
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>
--- event-booking/src/components/EventCard.vue
+++ event-booking/src/components/EventCard.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="border border-gray-200 bg-white rounded-md">
-    <h3 class="p-4 font-medium text-xl border-b border-gray-200">What</h3>
-    <p class="p-4 border-b border-gray-200">When</p>
-    <p class="p-4">Description</p>
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
+    <p class="p-4 border-b border-gray-200">{{ when }}</p>
+    <p class="p-4">{{ description }}</p>
   </div>
 </template>
+
+<script setup>
+defineProps({
+  title: String,
+  when: String,
+  description: String
+});
+</script>
