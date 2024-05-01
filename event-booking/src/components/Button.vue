4ea984afe7aca694173916b1444a80b328276a85
Piotr Jura <jura.piotrek@gmail.com>
2024-05-01 20:59:46 +0200

    0036-fall-through-attrs

---
+++ event-booking/src/components/Button.vue
@@ -0,0 +1,12 @@
+<template>
+  <button class="text-sm font-medium border border-gray-200 rounded-full px-3 py-1 hover:bg-gray-100" @click="$emit('click')">
+    {{label}}
+  </button>
+</template>
+
+<script setup>
+defineEmits(['click']);
+defineProps({
+  label: String
+});
+</script>
