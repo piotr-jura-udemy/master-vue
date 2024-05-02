093c5dd9dee19ac35e3134113988abe43505a78d
Piotr Jura <jura.piotrek@gmail.com>
2024-05-02 20:27:11 +0200

    0037-component-basic-slot

--- event-booking/src/components/EventCard.vue
+++ event-booking/src/components/EventCard.vue
@@ -4,17 +4,15 @@
     <p class="p-4 border-b border-gray-200">{{ when }}</p>
     <p class="p-4">{{ description }}</p>
     <section class="flex justify-end p-4">
-      <!-- <button class="text-sm font-medium border border-gray-200 rounded-full px-3 py-1 hover:bg-gray-100"
-        @click="$emit('register')">
+      <RoundButton @click="$emit('register')">
         Register
-      </button> -->
-      <Button label="Register" @click="$emit('register')" />
+      </RoundButton>
     </section>
   </div>
 </template>
 
 <script setup>
-import Button from "@/components/Button.vue";
+import RoundButton from "@/components/RoundButton.vue";
 defineProps({
   title: String,
   when: String,
