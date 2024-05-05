d75331423528ecbeee24ea60206508b8e4e24886
Piotr Jura <jura.piotrek@gmail.com>
2024-05-05 17:00:41 +0200

    0046-loading-state

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -2,7 +2,7 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8">
+    <section class="grid grid-cols-2 gap-8" v-if="!eventsLoading">
       <EventCard
         v-for="event in events"
         :key="event.id"
@@ -12,6 +12,7 @@
         @register="console.log('Registered!')"
       />
     </section>
+    <section v-else>Loading events...</section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-4">
       <BookingItem v-for="i in 3" :key="i" />
@@ -20,18 +21,21 @@
 </template>
 
 <script setup>
-import {ref, onMounted} from 'vue';
+import { ref, onMounted } from 'vue';
 import EventCard from '@/components/EventCard.vue';
 import BookingItem from '@/components/BookingItem.vue';
 
 const events = ref([]);
+const eventsLoading = ref(false);
 
 const fetchEvents = async () => {
-  const response = await fetch(
-    'http://localhost:3001/events'
-  );
-  events.value = await response.json();
-  console.log(events.value);
+  eventsLoading.value = true;
+  try {
+    const response = await fetch('http://localhost:3001/events');
+    events.value = await response.json();
+  } finally {
+    eventsLoading.value = false;
+  }
 };
 
 onMounted(() => fetchEvents());
