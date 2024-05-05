1f000847a8d513cb6d8f1cdc4a8f6f451cc3dcaa
Piotr Jura <jura.piotrek@gmail.com>
2024-05-05 18:57:25 +0200

    0047-loading-card

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -2,17 +2,21 @@
   <main class="container mx-auto my-8 space-y-8">
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
-    <section class="grid grid-cols-2 gap-8" v-if="!eventsLoading">
-      <EventCard
-        v-for="event in events"
-        :key="event.id"
-        :title="event.title"
-        :when="event.date"
-        :description="event.description"
-        @register="console.log('Registered!')"
-      />
+    <section class="grid grid-cols-2 gap-8">
+      <template v-if="!eventsLoading">
+        <EventCard
+          v-for="event in events"
+          :key="event.id"
+          :title="event.title"
+          :when="event.date"
+          :description="event.description"
+          @register="console.log('Registered!')"
+        />
+      </template>
+      <template v-else>
+        <LoadingEventCard v-for="i in 4" :key="i" />
+      </template>
     </section>
-    <section v-else>Loading events...</section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-4">
       <BookingItem v-for="i in 3" :key="i" />
@@ -23,6 +27,7 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import EventCard from '@/components/EventCard.vue';
+import LoadingEventCard from '@/components/LoadingEventCard.vue';
 import BookingItem from '@/components/BookingItem.vue';
 
 const events = ref([]);
---
+++ event-booking/src/components/LoadingEventCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <RoundedCard class="animate-pulse">
+    <div class="p-4 border-b border-gray-200 h-8 bg-gray-300"></div>
+    <div class="p-4 border-b border-gray-200 h-6 bg-gray-300"></div>
+    <div class="p-4 h-6 bg-gray-300"></div>
+
+    <div class="flex justify-end p-4">
+      <div class="h-8 w-24 bg-gray-300 rounded-full"></div>
+    </div>
+  </RoundedCard>
+</template>
+
+<script setup>
+import RoundedCard from '@/components/RoundedCard.vue';
+</script>
