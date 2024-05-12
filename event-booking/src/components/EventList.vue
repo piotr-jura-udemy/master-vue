8bba862be368417a4866deed234c4661f2eeaff1
Piotr Jura <jura.piotrek@gmail.com>
2024-05-12 17:05:25 +0200

    0058-errror-handling-and-recovery

--- event-booking/src/components/EventList.vue
+++ event-booking/src/components/EventList.vue
@@ -1,41 +1,57 @@
 <template>
-  <section class="grid grid-cols-2 gap-8">
-    <template v-if="!loading">
-      <template v-if="events.length">
-        <EventCard
-          v-for="event in events"
-          :key="event.id"
-          :title="event.title"
-          :when="event.date"
-          :description="event.description"
-          @register="$emit('register', event)"
-        />
+  <template v-if="error">
+    <SectionCard>
+      <div class="space-y-4 items-center flex flex-col">
+        <div class="text-red-500">Could not load events at the moment. Please try again.</div>
+        <RoundButton @click="fetchEvents">Retry now</RoundButton>
+      </div>
+    </SectionCard>
+  </template>
+  <template v-else>
+    <section class="grid grid-cols-2 gap-8">
+      <template v-if="!loading">
+        <template v-if="events.length">
+          <EventCard
+            v-for="event in events"
+            :key="event.id"
+            :title="event.title"
+            :when="event.date"
+            :description="event.description"
+            @register="$emit('register', event)"
+          />
+        </template>
+        <template v-else>
+          <div class="col-span-2 text-center text-gray-500">No events yet.</div>
+        </template>
       </template>
       <template v-else>
-        <div class="col-span-2 text-center text-gray-500">No events yet.</div>
+        <LoadingEventCard v-for="i in 4" :key="i" />
       </template>
-    </template>
-    <template v-else>
-      <LoadingEventCard v-for="i in 4" :key="i" />
-    </template>
-  </section>
+    </section>
+  </template>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import EventCard from '@/components/EventCard.vue';
 import LoadingEventCard from '@/components/LoadingEventCard.vue';
+import SectionCard from '@/components/SectionCard.vue';
+import RoundButton from './RoundButton.vue';
 
 defineEmits(['register']);
 
 const events = ref([]);
 const loading = ref(false);
+const error = ref(null);
 
 const fetchEvents = async () => {
   loading.value = true;
+  error.value = null;
   try {
     const response = await fetch('http://localhost:3001/events');
     events.value = await response.json();
+  } catch (e) {
+    error.value = e;
   } finally {
     loading.value = false;
   }
