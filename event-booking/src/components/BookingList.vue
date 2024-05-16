60f6db076b129cd1d8db9c854763775645185e83
Piotr Jura <jura.piotrek@gmail.com>
2024-05-16 18:36:14 +0200

    0066-errors-in-booking-list

--- event-booking/src/components/BookingList.vue
+++ event-booking/src/components/BookingList.vue
@@ -1,18 +1,23 @@
 <template>
-  <section class="grid grid-cols-1 gap-4">
-    <template v-if="!loading">
-      <BookingItem
-        v-for="booking in bookings"
-        :key="booking.id"
-        :title="booking.eventTitle"
-        :status="booking.status"
-        @cancelled="cancelBooking(booking.id)"
-      />
-    </template>
-    <template v-else>
-      <LoadingBookingItem v-for="i in 4" :key="i" />
-    </template>
-  </section>
+  <template v-if="error">
+    <ErrorCard :retry="fetchBookings">Failed to fetch bookings.</ErrorCard>
+  </template>
+  <template v-else>
+    <section class="grid grid-cols-1 gap-4">
+      <template v-if="!loading">
+        <BookingItem
+          v-for="booking in bookings"
+          :key="booking.id"
+          :title="booking.eventTitle"
+          :status="booking.status"
+          @cancelled="cancelBooking(booking.id)"
+        />
+      </template>
+      <template v-else>
+        <LoadingBookingItem v-for="i in 4" :key="i" />
+      </template>
+    </section>
+  </template>
 </template>
 
 <script setup>
@@ -20,8 +25,9 @@ import { onMounted } from 'vue';
 import LoadingBookingItem from '@/components/LoadingBookingItem.vue';
 import BookingItem from '@/components/BookingItem.vue';
 import useBookings from '@/composables/useBookings';
+import ErrorCard from '@/components/ErrorCard.vue';
 
-const { bookings, loading, fetchBookings, cancelBooking } = useBookings();
+const { bookings, loading, error, fetchBookings, cancelBooking } = useBookings();
 
 onMounted(() => {
   fetchBookings();
