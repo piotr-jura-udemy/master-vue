331a48704dd2d73904a9b53cb0cf7ae2aaa62b9f
Piotr Jura <jura.piotrek@gmail.com>
2024-05-20 20:16:22 +0200

    0071-rendering-tabs

--- vue-settings/src/App.vue
+++ vue-settings/src/App.vue
@@ -1,6 +1,19 @@
+<template>
+  <main class="max-w-2xl mx-auto px-4">
+    <nav class="font-medium text-center text-gray-500 border-b border-gray-200 mb-4">
+      <ul class="flex flex-wrap -mb-px">
+        <li v-for="tab in tabs" :key="tab.key">
+          <TabLink :tab="tab" :currentTab="currentTab" @click="currentTab = tab.key" />
+        </li>
+      </ul>
+    </nav>
+  </main>
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue';
 import type { Tab, TabKey } from './types';
+import TabLink from '@/components/TabLink.vue';
 
 const tabs: Tab[] = [
   {
@@ -18,7 +31,3 @@ const tabs: Tab[] = [
 ];
 const currentTab = ref<TabKey>('General');
 </script>
-
-<template>
-  <main>Hello</main>
-</template>
--- vue-settings/src/components/TabLink.vue
+++ vue-settings/src/components/TabLink.vue
@@ -1,9 +1,17 @@
-<template>Link</template>
+<template>
+  <button @click="$emit('click')"
+    :class="{ 'p-4 border-b-2': true, 'border-transparent': !isActive, 'border-b-blue-600 text-blue-600': isActive }">
+    {{ tab.label }}
+  </button>
+</template>
 
 <script setup lang="ts">
 import type { Tab } from '@/types';
+import { computed } from 'vue';
 
-defineProps<{
+const isActive = computed(() => props.currentTab === props.tab.key);
+
+const props = defineProps<{
   currentTab: string;
   tab: Tab
 }>();
