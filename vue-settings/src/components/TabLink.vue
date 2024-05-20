285c8c3e331cda7b6fea0472a5a13cf63786e038
Piotr Jura <jura.piotrek@gmail.com>
2024-05-20 15:10:33 +0200

    0069-ts-for-props-and-events

---
+++ vue-settings/src/components/TabLink.vue
@@ -0,0 +1,14 @@
+<template>Link</template>
+
+<script setup lang="ts">
+defineProps<{
+  currentTab: string;
+  tab: {
+    name: string;
+    label: string;
+  }
+}>();
+defineEmits<{
+  click: []
+}>();
+</script>
