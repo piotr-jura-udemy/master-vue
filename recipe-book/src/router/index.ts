9cb8acf43f1f83bf33edf8dd12ecbe3a76ac51aa
Piotr Jura <jura.piotrek@gmail.com>
2024-05-28 18:36:18 +0200

    0085-vue-router-setup

--- recipe-book/src/router/index.ts
+++ recipe-book/src/router/index.ts
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+console.log(import.meta.env.BASE_URL)
+
+// mydomain.com
+// mydomain.com/app
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
--- recipe-book/vite.config.ts
+++ recipe-book/vite.config.ts
@@ -6,10 +6,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
