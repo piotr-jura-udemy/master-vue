78f165542a7377c66ee73884c6dc8015d532ff63
Piotr Jura <jura.piotrek@gmail.com>
2024-05-30 17:53:49 +0200

    0091-programmatic-navigation

--- recipe-book/src/App.vue
+++ recipe-book/src/App.vue
@@ -12,6 +12,9 @@ import { RouterLink, RouterView } from 'vue-router';
         <li>
           <RouterLink :to="{ name: 'favorites' }">Favorites</RouterLink>
         </li>
+        <li>
+          <RouterLink :to="{ name: 'add-recipe' }">Add Recipe</RouterLink>
+        </li>
       </ul>
     </nav>
   </header>
--- recipe-book/src/views/AddRecipeView.vue
+++ recipe-book/src/views/AddRecipeView.vue
@@ -1,3 +1,21 @@
 <template>
   <div>Add form</div>
+  <form @submit.prevent="addRecipe">
+    <button type="submit">
+      Add
+    </button>
+  </form>
 </template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const addRecipe = () => {
+  router.push({
+    name: 'recipe',
+    params: {
+      id: 1
+    }
+  })
+}
+</script>
