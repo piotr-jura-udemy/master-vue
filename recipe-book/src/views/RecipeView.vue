9f414f071fc48c357fba24d27d3f4baa7a1a2982
Piotr Jura <jura.piotrek@gmail.com>
2024-05-30 19:23:49 +0200

    0092-data-fetching-on-mounted

--- recipe-book/src/views/RecipeView.vue
+++ recipe-book/src/views/RecipeView.vue
@@ -2,12 +2,28 @@
   <div>
     Recipe {{ $route.params.id }}
   </div>
+  <nav>
+    <ul>
+      <li>
+        <RouterLink :to="{ name: 'recipe', params: { id: 1 } }">Recipe 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'recipe', params: { id: 3 } }">Recipe 3</RouterLink>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
+// /recipe/1
+// /recipe/2
+import { onMounted, watch } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
 
 const route = useRoute();
 
 console.log(route.params.id);
+
+// onMounted(() => console.log(`Fetching data inside onMounted`));
+watch(() => route.params.id, () => console.log(`Fetching inside watch`), { immediate: true });
 </script>
