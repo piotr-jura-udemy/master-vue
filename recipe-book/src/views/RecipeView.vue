d47dbca31ac232e6cf4177e050c2ed2aa1eedacb
Piotr Jura <jura.piotrek@gmail.com>
2024-09-19 17:29:04 +0200

    0108-setting-up-project

--- recipe-book/src/views/RecipeView.vue
+++ recipe-book/src/views/RecipeView.vue
@@ -3,9 +3,14 @@
     <h1 class="text-2xl font-bold mb-4">{{ recipe?.name }}</h1>
     <p class="mb-4">{{ recipe?.description }}</p>
     <div class="flex items-center gap-4">
-      <RouterLink :to="{ name: 'edit-recipe', params: { id: recipe?.id } }" class="hover:underline">Edit</RouterLink>
-      <button v-if="recipe" @click="recipeStore.toggleFavorite(recipe.id)"
-        class="px-4 py-2 bg-orange-600 text-white rounded hover:bg-orange-700">
+      <RouterLink :to="{ name: 'edit-recipe', params: { id: recipe?.id } }" class="hover:underline"
+        >Edit</RouterLink
+      >
+      <button
+        v-if="recipe"
+        @click="recipeStore.toggleFavorite(recipe.id)"
+        class="px-4 py-2 bg-orange-600 text-white rounded hover:bg-orange-700"
+      >
         {{ isFavorite ? 'Remove from favorites' : 'Add to favorites' }}
       </button>
     </div>
@@ -21,7 +26,5 @@ const route = useRoute();
 const recipeStore = useRecipeStore();
 
 const recipe = computed(() => recipeStore.getRecipeById(route.params.id as string));
-const isFavorite = computed(
-  () => recipe.value ? recipeStore.isFavorite(recipe.value.id) : false
-);
+const isFavorite = computed(() => (recipe.value ? recipeStore.isFavorite(recipe.value.id) : false));
 </script>
