e052b9f9a964716ffc8accb464ebf87773d5d0da
Piotr Jura <jura.piotrek@gmail.com>
2024-09-19 19:06:00 +0200

    0109-the-page-shell

--- trello-clone/src/App.vue
+++ trello-clone/src/App.vue
@@ -1,5 +1,89 @@
 <template>
   <main class="p-5 font-sans">
-    Hello
+    <div class="flex gap-5 py-5 overflow-x-auto">
+      <div class="bg-gray-100 p-3 rounded-lg min-w-[250px] flex flex-col">
+        <h2 class="font-medium mb-2">The List</h2>
+        <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">
+            Card Description
+          </p>
+        </div>
+
+        <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">
+            Card Description
+          </p>
+        </div>
+
+        <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">
+            Card Description
+          </p>
+        </div>
+
+        <button class="w-full bg-transparent rounded hover:bg-white text-gray-500 p-2 text-left mt-2 text-sm font-medium">
+          + Add Card
+        </button>
+      </div>
+
+      <div class="bg-gray-100 p-3 rounded-lg min-w-[250px] flex flex-col">
+        <h2 class="font-medium mb-2">The List</h2>
+        <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">
+            Card Description
+          </p>
+        </div>
+
+        <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">
+            Card Description
+          </p>
+        </div>
+
+        <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">
+            Card Description
+          </p>
+        </div>
+
+        <button class="w-full bg-transparent rounded hover:bg-white text-gray-500 p-2 text-left mt-2 text-sm font-medium">
+          + Add Card
+        </button>
+      </div>
+
+      <div class="bg-gray-100 p-3 rounded-lg min-w-[250px] flex flex-col">
+        <h2 class="font-medium mb-2">The List</h2>
+        <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">
+            Card Description
+          </p>
+        </div>
+
+        <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">
+            Card Description
+          </p>
+        </div>
+
+        <div class="bg-white p-2 my-2 rounded shadow cursor-pointer">
+          <span class="text-sm font-medium">Title Card</span>
+          <p class="text-xs text-gray-400">
+            Card Description
+          </p>
+        </div>
+
+        <button class="w-full bg-transparent rounded hover:bg-white text-gray-500 p-2 text-left mt-2 text-sm font-medium">
+          + Add Card
+        </button>
+      </div>
+    </div>
   </main>
 </template>
